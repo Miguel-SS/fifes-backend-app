b9fd41ebf0729fa47e6ec1ac0a288e5b61d47cf9
Miguel-SS <55169938+Miguel-SS@users.noreply.github.com>
2023-08-17 16:53:30 -0600

    custom query findAllById

--- src/main/kotlin/com/backend/fifes/Repositories.kt
+++ src/main/kotlin/com/backend/fifes/Repositories.kt
@@ -1,9 +1,12 @@
 package com.backend.fifes
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.*
+import javax.transaction.Transactional
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
@@ -19,7 +22,15 @@ interface RoleRepository : JpaRepository<Role, Long> {
 interface PrivilegeRepository : JpaRepository<Privilege, Long>
 
 @Repository
-interface PlayerRepository : JpaRepository<Player, Long>
+interface PlayerRepository : JpaRepository<Player, Long> {
+    @Transactional
+    @Modifying
+    @Query(
+        value = "SELECT * FROM public.player WHERE id IN :ids",
+        nativeQuery = true
+    )
+    fun findAllById(@Param("ids") ids: List<Long>): MutableList<Player>
+}
 
 @Repository
 interface StatsRepository : JpaRepository<Stats, Long>
